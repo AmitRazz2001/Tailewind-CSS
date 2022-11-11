813652bc52a00481e72f0daf4067c8fee1ea24ff
amitkumar2001 <amit.kumar@staqo.com>
2022-11-11 12:05:59 +0530

    facebook clone and tailwind setup template

---
+++ tw-4-facebook/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      margin: {
+        '54rem': '54rem',
+      },
+      fontSize: {
+        sm: '0.9rem',
+        base: '1rem',
+        xl: '1.25rem',
+
+      }
+    },
+  },
+  plugins: [],
+}
